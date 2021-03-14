3e3087de4fc78a15be6cd211550933b82114ece5
Sid <sidmirza4@gmail.com>
2021-03-14 10:36:52 +0530

    show notification for comments

--- components/input/comments.js
+++ components/input/comments.js
@@ -1,50 +1,78 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
 
 import CommentList from './comment-list';
 import NewComment from './new-comment';
+import NotificationContext from '../../store/notification-context';
 import classes from './comments.module.css';
 
 function Comments(props) {
-  const { eventId } = props;
+	const { eventId } = props;
+	const notificationCtx = useContext(NotificationContext);
 
-  const [showComments, setShowComments] = useState(false);
-  const [comments, setComments] = useState([]);
+	const [showComments, setShowComments] = useState(false);
+	const [comments, setComments] = useState([]);
 
-  useEffect(() => {
-    if (showComments) {
-      fetch('/api/comments/' + eventId)
-        .then((response) => response.json())
-        .then((data) => {
-          setComments(data.comments);
-        });
-    }
-  }, [showComments]);
+	useEffect(() => {
+		if (showComments) {
+			fetch('/api/comments/' + eventId)
+				.then((response) => response.json())
+				.then((data) => {
+					setComments(data.comments);
+				})
+				.catch((error) => {});
+		}
+	}, [showComments]);
 
-  function toggleCommentsHandler() {
-    setShowComments((prevStatus) => !prevStatus);
-  }
+	function toggleCommentsHandler() {
+		setShowComments((prevStatus) => !prevStatus);
+	}
 
-  function addCommentHandler(commentData) {
-    fetch('/api/comments/' + eventId, {
-      method: 'POST',
-      body: JSON.stringify(commentData),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data));
-  }
+	function addCommentHandler(commentData) {
+		notificationCtx.showNotification({
+			title: 'Adding Comment',
+			message: 'Your comment is being added',
+			status: 'pending',
+		});
+		fetch('/api/comments/' + eventId, {
+			method: 'POST',
+			body: JSON.stringify(commentData),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		})
+			.then(async (response) => {
+				if (response.ok) {
+					return response.json();
+				}
 
-  return (
-    <section className={classes.comments}>
-      <button onClick={toggleCommentsHandler}>
-        {showComments ? 'Hide' : 'Show'} Comments
-      </button>
-      {showComments && <NewComment onAddComment={addCommentHandler} />}
-      {showComments && <CommentList items={comments} />}
-    </section>
-  );
+				const data = await response.json();
+				throw new Error(data.message || 'Something went wrong');
+			})
+			.then(() => {
+				notificationCtx.showNotification({
+					title: 'Comment Added',
+					message: 'Your comment has been added.',
+					status: 'success',
+				});
+			})
+			.catch((err) => {
+				notificationCtx.showNotification({
+					title: 'Failed!',
+					message: err.message || 'Something went wrong.',
+					status: 'error',
+				});
+			});
+	}
+
+	return (
+		<section className={classes.comments}>
+			<button onClick={toggleCommentsHandler}>
+				{showComments ? 'Hide' : 'Show'} Comments
+			</button>
+			{showComments && <NewComment onAddComment={addCommentHandler} />}
+			{showComments && <CommentList items={comments} />}
+		</section>
+	);
 }
 
 export default Comments;
